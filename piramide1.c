9fb9a7f7c0572c358f7127224135b7ca5c8bab78
limenaLinda <104432227+limenaLinda@users.noreply.github.com>
2022-12-12 15:42:10 -0300

    Add files via upload

---
+++ piramide1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+    int n, x=1 ;
+    
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+        {
+            for(int j = 0; j <= i; j++)
+                {
+                    printf("%d ",x);
+                    x++;
+                    
+                }
+            printf("\n");
+        }
+    return 0;
+}
