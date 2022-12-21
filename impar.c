f1afaddca88aeeaf45e88ad26c992e772574c3fa
limenaLinda <104432227+limenaLinda@users.noreply.github.com>
2022-12-21 12:33:31 -0300

    Add files via upload

--- impar.c
+++ impar.c
@@ -1,70 +1,36 @@
 #include <stdio.h>
+int main(void){
+    int num;
 
-int main(){
+    scanf("%d",&num);
+    int vetor[num],vetorpar[num],vetorimpar[num];
+    int tam_par=0, tam_impar=0;
 
-    int escolhaparticipanteA,n1j1,n2j1,escolhapC, n1j2,n2j2,escolhapAB,n1j3,n2j3,soma;
-    char v1,v2,v3;
-    char pa='A';
-    char pb= 'B';
-    char pc ='C';
-    char pd = 'D';
+    int a=0;
+    int b=0;
 
 
-    scanf("%i %i %i",&escolhaparticipanteA, &n1j1,  &n2j1);
-    scanf("%i %i %i",&escolhapC, &n1j2,  &n2j2);
-    scanf("%i %i %i",&escolhapAB, &n1j3,  &n2j3);
-
-    soma= n1j1+n2j1;
-    if((soma%2 )!= 0){
-        if(escolhaparticipanteA==1){
-            v1=pa;
-        } else {
-            v1=pb;
+    for(int i =0 ;i<num;i++)
+    {
+        scanf("%d",&vetor[i]);
+        if(vetor[i]%2==0){
+            vetorpar[a]=vetor[i];
+            a++;
+            tam_par++;
         }
-    } else{
-        if(escolhaparticipanteA==1){
-            v1= pb;
-        } else {
-            v1= pa;
+        if(vetor[i]%2!=0){
+            vetorimpar[b]=vetor[i];
+            b++;
+            tam_impar++;
         }
     }
-    soma=n1j2+n2j2;
-     if((soma%2 )!= 0){
-        if(escolhapC==1){
-            v2= pc;
-        } else {
-            v2= pd;
-        }
-    } else{
-        if(escolhapC==1){
-            v2= pd;
-        } else {
-            v2= pc;
-        }
+    for(int i =0 ;i<tam_par;i++){
+        printf("%d ",vetorpar[i]);
+    }
+    printf("\n");
+    for(int i =0 ;i<tam_impar;i++){
+        printf("%d ",vetorimpar[i]);
     }
 
-
-    //printf("%c %c\n", v1, v2);
-
-    //return 0;
-
-    soma= n1j3+n2j3;
-    if((soma%2 )!= 0){
-        if(escolhapAB==1){
-            v3=v1;
-        } else {
-            v3=v2;
-        }
-    }else{
-        if(escolhapAB==1)
-        v3=v2;
-        else{
-            v3=v1;
-        }
-    }
-
-    //printf("%c %c %c\n", v1, v2,v3);
-    printf("%c\n",v3);
-    
     return 0 ;
-}
+    }
